0e6bdf15f75f06b08673af08433b162e6e71f32a
Alexis Murzeau <amubtdx@gmail.com>
2017-11-16 22:53:39 +0100

    Update build dir to ../build-area

--- travis-build/deploy.sh
+++ travis-build/deploy.sh
@@ -10,5 +10,5 @@ echo "Deploying on bintray repository $bintray_repository"
 
 CHROOT_SESSION=$(sudo schroot -c "${CHROOT_NAME}" --begin-session)
 sudo schroot --run-session -c $CHROOT_SESSION -u root -- apt-get install -y --no-install-recommends python3 python3-debian python3-requests
-sudo -E schroot -p --run-session -c $CHROOT_SESSION -u $USER -- python3 $(dirname "$0")/bintray_upload_changes.py amurzeau $bintray_repository $CHROOT_ALIAS build-dir/*.changes
+sudo -E schroot -p --run-session -c $CHROOT_SESSION -u $USER -- python3 $(dirname "$0")/bintray_upload_changes.py amurzeau $bintray_repository $CHROOT_ALIAS ${CHANGES_FILE}
 sudo schroot --end-session -c $CHROOT_SESSION
