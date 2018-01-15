b4818e00dcc13711ce6aceb4419e1a873170ee80
Alexis Murzeau <amubtdx@gmail.com>
2018-01-15 22:27:08 +0100

    Remove scripts from docker intermediate image

--- docker-sbuild-unstable/prepare.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -x
-
-apt-get update &&
-apt-get install -y --no-install-recommends sbuild schroot gnupg lintian autopkgtest dpkg-dev git-buildpackage pristine-tar &&
-apt-get clean
-
