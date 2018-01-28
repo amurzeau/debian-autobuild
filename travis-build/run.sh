0bf63766c9f3a2d8ed368f999f6ed3000c060f24
Alexis Murzeau <amubtdx@gmail.com>
2018-01-28 13:27:35 +0100

    Always upload source tarball

--- travis-build/run.sh
+++ travis-build/run.sh
@@ -9,5 +9,5 @@ docker pull amubtdx/debian-sbuild-$DIST
 
 docker run --rm --privileged -v $PWD/../build-area:/build/build-area -v $SOURCE_DIR:/build/source -w /build/source -it amubtdx/debian-sbuild-$DIST \
 	bash -c "apt-get -y update && apt-get -y dist-upgrade && apt-get -y autoremove && \
-       gbp buildpackage --git-verbose --git-ignore-branch --git-cleaner= \"--git-builder=sbuild -v -As -d $DIST --no-clean-source --run-lintian --lintian-opts=\\\"-EviIL +pedantic\\\" --run-autopkgtest --autopkgtest-root-args= --autopkgtest-opts=\\\"-- schroot %r-%a-sbuild\\\"\""
+       gbp buildpackage --git-verbose --git-ignore-branch --git-cleaner= \"--git-builder=sbuild -v -As --force-orig-source -d $DIST --no-clean-source --run-lintian --lintian-opts=\\\"-EviIL +pedantic\\\" --run-autopkgtest --autopkgtest-root-args= --autopkgtest-opts=\\\"-- schroot %r-%a-sbuild\\\"\""
 
