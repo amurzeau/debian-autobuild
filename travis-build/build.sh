7272c4d2aaf6b20f3d876b76f674b83e914cc078
Alexis Murzeau <amubtdx@gmail.com>
2018-02-10 12:56:30 +0100

    Update autopkgtest testbed when running

--- travis-build/build.sh
+++ travis-build/build.sh
@@ -11,4 +11,4 @@ apt-get -y update &&
 	dch -M -l '~bintray' -u low "Rebuild for bintray" -D $DIST &&
 	git add debian/rules debian/changelog &&
 	git -c user.name='travis build' -c user.email='travis@example.com' commit -m "Rebuild for bintray" &&
-	gbp buildpackage --git-verbose --git-ignore-branch --git-cleaner= "--git-builder=sbuild -v -As -d $DIST --no-clean-source --run-lintian --lintian-opts=\"-EviIL +pedantic\" --run-autopkgtest --autopkgtest-root-args= --autopkgtest-opts=\"-- schroot %r-%a-sbuild\""
+	gbp buildpackage --git-verbose --git-ignore-branch --git-cleaner= "--git-builder=sbuild -v -As -d $DIST --no-clean-source --run-lintian --lintian-opts=\"-EviIL +pedantic\" --run-autopkgtest --autopkgtest-root-args= --autopkgtest-opts=\"--apt-upgrade -- schroot %r-%a-sbuild\""
